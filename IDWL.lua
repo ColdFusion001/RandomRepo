e41012fe63ef4506016078706b3de8d955b21a5e
ColdFusion001 <38503993+ColdFusion001@users.noreply.github.com>
2020-01-13 17:16:47 -0500

    Update IDWL.lua

--- IDWL.lua
+++ IDWL.lua
@@ -1,22 +1,6 @@
 
 userids = {
 [460444] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true,
-[] = true
 }
 return userids
--- local id = game.Players.LocalPlayer.UserId
+
