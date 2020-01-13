5a22a2b8e2305e5d320eeeda4a409b1c8729aecd
ColdFusion001 <38503993+ColdFusion001@users.noreply.github.com>
2020-01-13 16:47:52 -0500

    Update IDWL.lua

--- IDWL.lua
+++ IDWL.lua
@@ -1,6 +1,6 @@
 
 userids = {
-[460444] = true, -- this is my userid, remove it and add as much as you want in it.
+[460444] = true,
 [] = true,
 [] = true,
 [] = true,
@@ -20,9 +20,3 @@ userids = {
 }
 return userids
 -- local id = game.Players.LocalPlayer.UserId
-local whitelistecheck = loadstring(game:HttpGet("https://pastebin.com/raw/IDHERE", true))()
-if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
--- YOUR WHITELISTED SCRIPT HERE
-else
-game:service('Players').LocalPlayer:Kick('No auth contact staff')
-end
